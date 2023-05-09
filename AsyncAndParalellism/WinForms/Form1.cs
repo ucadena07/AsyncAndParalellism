8dd041ca0c86455e0e6435c74dcc63e911fbcdfd
ucadena07 <ucadena07@gmail.com>
2023-05-09 07:06:41 -0500

    semaporeslim

--- AsyncAndParalellism/WinForms/Form1.cs
+++ AsyncAndParalellism/WinForms/Form1.cs
@@ -66,28 +66,30 @@ namespace WinForms
 
         async Task ProcessCards(List<string> cards)
         {
+            using var semaphore = new SemaphoreSlim(250);
             var tasks = new List<Task<HttpResponseMessage>>();
 
-            //foreach (var card in cards)
-            //{
-            //    var json = JsonSerializer.Serialize(card);
-            //    var content = new StringContent(json, Encoding.UTF8, "application/json");
-            //    var responseTask = _httpClient.PostAsync($"{_baseUrl}/cards",content);
-
-            //    tasks.Add(responseTask);    
-            //}
-
-            await Task.Run( () =>
+            tasks = cards.Select(async card =>
             {
-                foreach (var card in cards)
-                {
-                    var json = JsonSerializer.Serialize(card);
-                    var content = new StringContent(json, Encoding.UTF8, "application/json");
-                    var responseTask = _httpClient.PostAsync($"{_baseUrl}/cards", content);
+                var json = JsonSerializer.Serialize(card);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-                    tasks.Add(responseTask);
+                await semaphore.WaitAsync();
+
+                try
+                {
+                    return await _httpClient.PostAsync($"{_baseUrl}/cards", content);
                 }
-            });
+                finally
+                {
+
+                    semaphore.Release();
+                }
+  
+            }).ToList();
+
+
+
 
             await Task.WhenAll(tasks);
         }
@@ -135,4 +137,34 @@ namespace WinForms
 //        var greeting = await response.Content.ReadAsStringAsync();
 //        return greeting;
 //    }
+//}
+
+
+// LESSON 18 - 19 
+//async Task ProcessCards(List<string> cards)
+//{
+//    var tasks = new List<Task<HttpResponseMessage>>();
+
+//    //foreach (var card in cards)
+//    //{
+//    //    var json = JsonSerializer.Serialize(card);
+//    //    var content = new StringContent(json, Encoding.UTF8, "application/json");
+//    //    var responseTask = _httpClient.PostAsync($"{_baseUrl}/cards",content);
+
+//    //    tasks.Add(responseTask);    
+//    //}
+
+//    await Task.Run(() =>
+//    {
+//        foreach (var card in cards)
+//        {
+//            var json = JsonSerializer.Serialize(card);
+//            var content = new StringContent(json, Encoding.UTF8, "application/json");
+//            var responseTask = _httpClient.PostAsync($"{_baseUrl}/cards", content);
+
+//            tasks.Add(responseTask);
+//        }
+//    });
+
+//    await Task.WhenAll(tasks);
 //}
