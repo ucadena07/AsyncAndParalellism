2c6f45ab4802b44488d08488c8e47fb6710e7d2a
Ulises Cadena <ucadena07@gmail.com>
2023-05-26 06:36:22 -0500

    maximum degree of parallelism

--- AsyncAndParalellism/WinForms/Form1.cs
+++ AsyncAndParalellism/WinForms/Form1.cs
@@ -30,17 +30,22 @@ namespace WinForms
 
             lodingGif.Visible = true;
 
-            var currentDict = AppDomain.CurrentDomain.BaseDirectory;
-            var originalFolder = Path.Combine(currentDict, @"images\result-seq");
-            var destSem = Path.Combine(currentDict, @"images\foreach-sequential");
-            var destParl = Path.Combine(currentDict, @"images\foreach-parallel");
-         
+            for (int i = 1; i <= 12; i++)
+            {
+                await DoMatricesTest(i);
+            }
 
-            Console.WriteLine("BEGIN");
 
-            //var images = GetImages();
-            var files = Directory.EnumerateFiles(originalFolder);
 
+
+
+            lodingGif.Visible = false;
+
+        }
+
+        async Task DoMatricesTest(int maxDegreeOfParalelism)
+        {
+            _cts = new CancellationTokenSource();
 
             var columnMatrixA = 1100;
             var rows = 1000;
@@ -51,43 +56,44 @@ namespace WinForms
             var stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            Action multipleMatrices = () => Matrices.MultiplyMatricesParallel(matrixA, matrixB,result);
 
-            Action tranformImages = () =>
+            try
             {
-                foreach (var image in files)
+                await Task.Run(() =>
                 {
-                    FlipImage(image, destSem);
-                }
-            };
+                    Matrices.MultiplyMatricesParallel(matrixA, matrixB, result, _cts.Token, maxDegreeOfParalelism);
+                });
+                Console.WriteLine($"With max degree of {maxDegreeOfParalelism} it took: {stopwatch.ElapsedMilliseconds /1000.0} seconds");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                _cts.Dispose();
+            }
+
 
             //sequential
-            multipleMatrices();
-            tranformImages();
+
 
 
             var timeSeq = stopwatch.ElapsedMilliseconds / 1000.0;
-            Console.WriteLine("Sequential - duration {0} seconds", timeSeq);
+            //Console.WriteLine("Sequential - duration {0} seconds", timeSeq);
             stopwatch.Restart();
 
             //parallerl
-            PrepareExecution(destSem, destParl);
-            Parallel.Invoke(multipleMatrices, tranformImages);  
+
 
 
 
             var timeSim = stopwatch.ElapsedMilliseconds / 1000.0;
-            Console.WriteLine("Simultaneous - duration {0} seconds", timeSim);
+            //Console.WriteLine("Simultaneous - duration {0} seconds", timeSim);
 
 
-            WriteComparison(timeSeq, timeSim);
-
-
-
-
-
-            lodingGif.Visible = false;
-
+            //WriteComparison(timeSeq, timeSim);
         }
 
         private void FlipImage(string file, string destinationDirectory)
@@ -1005,6 +1011,77 @@ private async void btnStart_Click_1(object sender, EventArgs e)
 
         }
  * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+/* Parallel invoke 
+ *        private async void btnStart_Click_1(object sender, EventArgs e)
+        {
+
+            lodingGif.Visible = true;
+
+            var currentDict = AppDomain.CurrentDomain.BaseDirectory;
+            var originalFolder = Path.Combine(currentDict, @"images\result-seq");
+            var destSem = Path.Combine(currentDict, @"images\foreach-sequential");
+            var destParl = Path.Combine(currentDict, @"images\foreach-parallel");
+         
+
+            Console.WriteLine("BEGIN");
+
+            //var images = GetImages();
+            var files = Directory.EnumerateFiles(originalFolder);
+
+
+            var columnMatrixA = 1100;
+            var rows = 1000;
+            var columnMatrixB = 1750;
+            var matrixA = Matrices.InitializeMatrix(rows, columnMatrixA);
+            var matrixB = Matrices.InitializeMatrix(columnMatrixA, columnMatrixB);
+            var result = new double[rows, columnMatrixB];
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Action multipleMatrices = () => Matrices.MultiplyMatricesParallel(matrixA, matrixB,result);
+
+            Action tranformImages = () =>
+            {
+                foreach (var image in files)
+                {
+                    FlipImage(image, destSem);
+                }
+            };
+
+            //sequential
+            multipleMatrices();
+            tranformImages();
+
+
+            var timeSeq = stopwatch.ElapsedMilliseconds / 1000.0;
+            Console.WriteLine("Sequential - duration {0} seconds", timeSeq);
+            stopwatch.Restart();
+
+            //parallerl
+            PrepareExecution(destSem, destParl);
+            Parallel.Invoke(multipleMatrices, tranformImages);  
+
+
+
+            var timeSim = stopwatch.ElapsedMilliseconds / 1000.0;
+            Console.WriteLine("Simultaneous - duration {0} seconds", timeSim);
+
+
+            WriteComparison(timeSeq, timeSim);
+
+
+
+
+
+            lodingGif.Visible = false;
+
+        }
  * 
  * 
  * 
